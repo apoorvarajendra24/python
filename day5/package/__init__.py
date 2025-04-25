64e3358c48dc7516d4561be061ac65535f0a07e5
ApoorvaBarapatre <apoorva.patrey22@gmail.com>
2025-04-25 12:38:17 +0530

    Add files via upload

---
+++ day5/package/__init__.py
@@ -0,0 +1,3 @@
+""" __init__.py is a special Python file used to mark a directory as a Python package. 
+It plays a role in Python's module and package system. 
+When you import a module from a directory, Python uses the __init__.py file to initialize the package."""
