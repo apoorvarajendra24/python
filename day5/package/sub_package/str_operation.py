533c82f0380b7b0ac5fd64dc28a3d8a0c794abdc
ApoorvaBarapatre <apoorva.patrey22@gmail.com>
2025-04-25 12:39:59 +0530

    Add files via upload

---
+++ day5/package/sub_package/str_operation.py
@@ -0,0 +1,14 @@
+def str_len(str):
+    return len(str)
+
+def str_concate(str1,str2):
+    return str1 + str2
+
+def str_reverse(str):
+    return str[::-1]
+
+def str_upper(str):
+    return str.upper()
+
+def str_lower(str):
+    return str.lower()
